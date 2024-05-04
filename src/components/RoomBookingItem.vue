74e1e181cdd1251ae79854567b4c23f54bd9d050
manhIoi <manhloi0505@gmail.com>
2024-05-04 23:17:03 +0700

    fix: style item with card

--- src/components/RoomBookingItem.vue
+++ src/components/RoomBookingItem.vue
@@ -15,39 +15,44 @@ const bookingItem = {
 </script>
 
 <template>
-  <q-card flat class="booking-item-container bg-white">
-    <div class="row">
-      <div class="col-xs-12 col-sm-6">
-        <q-img class="full-height full-width" :src="bookingItem.image" :alt="`booking_item_${bookingItem.id}`" />
-      </div>
-      <div class="col-xs-12 col-sm-6">
-        <div class="q-pa-lg">
+  <q-card flat class="booking-item-container bg-white row">
+    <div class="col-xs-12 col-sm-6">
+      <q-img class="full-width full-height" :src="bookingItem.image" :alt="`booking_item_${bookingItem.id}`"/>
+    </div>
+    <div class="col-xs-12 col-sm-6">
+      <div class="q-pa-md">
+        <q-card-section>
           <div class="column items-start">
-            <div class="q-pa-sm row flex-center booking-item-content_btn text-primary">
-              <p class="text-weight-bolder text-subtitle1 q-mr-xs">${{bookingItem.amount}}</p>
+            <div class="q-pa-sm row flex-center booking-item-content_btn text-primary q-mb-lg">
+              <p class="text-weight-bolder text-subtitle1 q-mr-xs">${{ bookingItem.amount }}</p>
               <p class="text-weight-medium text-body-2">Per night</p>
             </div>
-            <p class="text-h4 text-weight-bold q-pb-lg"> {{bookingItem.name}}</p>
-            <div class="row q-pb-lg">
-              <div class="row flex-center bg-grey-1 br-xs q-pa-sm q-mr-lg text-grey-9" >
-                <q-icon name="person" class="q-mr-xs" size="24px" />
-                <p>{{bookingItem.adultNumber}} Adults</p>
+            <p class="text-h4 text-weight-bold q-mb-lg"> {{ bookingItem.name }}</p>
+            <div class="row q-mb-lg">
+              <div class="row flex-center bg-grey-1 br-xs q-pa-sm q-mr-lg text-grey-9">
+                <q-icon name="person" class="q-mr-xs" size="24px"/>
+                <p>{{ bookingItem.adultNumber }} Adults</p>
               </div>
               <div class="row flex-center bg-grey-1 br-xs q-pa-sm q-mr-lg text-grey-9">
-                <q-icon name="person" class="q-mr-xs" size="24px" />
-                <p>{{bookingItem.adultNumber}} Adults</p>
+                <q-icon name="person" class="q-mr-xs" size="24px"/>
+                <p>{{ bookingItem.adultNumber }} Adults</p>
               </div>
-
               <div class="row flex-center bg-grey-1 br-xs q-pa-sm q-mr-lg text-grey-9">
-                <q-icon name="person" class="q-mr-xs" size="24px" />
-                <p>{{bookingItem.adultNumber}} Adults</p>
+                <q-icon name="person" class="q-mr-xs" size="24px"/>
+                <p>{{ bookingItem.adultNumber }} Adults</p>
               </div>
             </div>
-            <q-separator inset spaced color="orange" />
-            <div>hello</div>
           </div>
-        </div>
+          <q-separator class="q-mb-lg" />
+          <p class="text-grey-8 text-body2 q-mb-lg">{{ bookingItem.description }}</p>
+          <q-btn color="primary" size="lg" class="q-px-xl br-sm">
+            <p class="text-white text-weight-bold">
+              Book now
+            </p>
+          </q-btn>
+        </q-card-section>
       </div>
+
     </div>
   </q-card>
 </template>
@@ -57,8 +62,13 @@ const bookingItem = {
   border-radius: 8px;
   overflow: hidden;
 }
+
 .booking-item-content_btn {
   border-radius: 4px;
   border: 2px solid var(--primary-color);
+}
+
+.booking-item-container-wrapper {
+  padding: 0 !important;
 }
 </style>
